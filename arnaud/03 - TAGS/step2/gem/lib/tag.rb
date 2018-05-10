9eb3977b4c3d5eec3defb2d8feb38ec57e1d7dcc
Nicolas Le Chenic <nicolaslechenic@gmail.com>
2018-05-10 15:32:57 +0200

    add tags step2

---
+++ arnaud/03 - TAGS/step2/gem/lib/tag.rb
@@ -0,0 +1,8 @@
+module Shopify
+  class Tag
+    attr_accessor :label
+    def initialize(label)
+      @label = label
+    end
+  end
+end
---
+++ arnaud/03 - TAGS/step2/gem/spec/tag_spec.rb
@@ -0,0 +1,7 @@
+require 'pry'
+require './shopify'
+require './lib/tag'
+
+RSpec.describe Shopify::Product do
+  # TODO
+end
